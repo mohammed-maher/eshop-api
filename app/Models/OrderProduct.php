816cc1717e482e4208d66fa27ef37efee7d06458
Mohammed Maher <mohammed.m.salloom@gmail.com>
2021-06-28 20:01:15 +0300

    adds order product fillables and relations

--- app/Models/OrderProduct.php
+++ app/Models/OrderProduct.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class OrderProduct extends Model
 {
     use HasFactory;
+    protected $fillable = ['order_id','product_id','quantity','product_weight_id'];
+
+    public function order(){
+        $this->belongsTo(Order::class);
+    }
+
+    public function product(){
+        $this->belongsTo(Product::class);
+    }
 }
