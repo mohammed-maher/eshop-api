976ba92215a8ad7b15e3ee34576e1e0341131aa7
Mohammed Maher <mohammed.m.salloom@gmail.com>
2021-06-29 18:33:39 +0300

    adds order product resource and collection

---
+++ app/Http/Resources/OrderProductResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+          'product'=>new ProductResource($this->product),
+          'quantity'=>$this->quantity,
+          'weight'=>$this->weight
+        ];
+    }
+}
